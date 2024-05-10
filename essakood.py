5061cb01c6c7867edc27a15656f8eb81f282beed
Johanna Simm <johanna.simm@gmail.com>
2024-05-10 14:51:29 +0300

    muudatused

--- essakood.py
+++ essakood.py
@@ -1,3 +1,5 @@
+import random
+
 # erinevad inimesed
 johannapapp = {"nimi": "Johanna Papp", "suund":"progepets", "sugu":"tütarlaps", "juuksed": "pruun", "aasta":"2006", "silmad":"pruunid", "prillid": "Ei", "läätsed": "Ei", "aastaaeg":"sügis", "värv":"roheline"}
 johannasimm = {"nimi": "Johanna Simm", "suund":"progepets", "sugu":"tütarlaps", "juuksed": "pruun", "aasta":"2007","silmad":"sinakashallid", "prillid":"Ei", "läätsed": "Ei", "aastaeg": "kevad", "värv":"lilla"}
@@ -8,3 +10,18 @@ kogumik = []
 # lisab neid
 kogumik.append(johannapapp)
 kogumik.append(johannasimm)
+
+f = open("kysimused.txt")
+
+# kysimuse valimine
+kyssad = []
+for rida in f:
+    k = f.readline()
+    kyssad.append(k)
+
+
+kysimus = random.choice(kyssad)
+
+print(kysimus)
+kyssad.remove(kysimus)
+vastus = input("Vasta 'Jah' või 'Ei':")
