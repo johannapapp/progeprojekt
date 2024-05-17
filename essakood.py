046f56586387e5e2c36479c34f5665833275e149
Johanna Simm <johanna.simm@gmail.com>
2024-05-17 14:16:19 +0300

    kood

--- essakood.py
+++ essakood.py
@@ -1,27 +1,14 @@
-import random
-
-# erinevad inimesed
-johannapapp = {"nimi": "Johanna Papp", "suund":"progepets", "sugu":"tütarlaps", "juuksed": "pruun", "aasta":"2006", "silmad":"pruunid", "prillid": "Ei", "läätsed": "Ei", "aastaaeg":"sügis", "värv":"roheline"}
-johannasimm = {"nimi": "Johanna Simm", "suund":"progepets", "sugu":"tütarlaps", "juuksed": "pruun", "aasta":"2007","silmad":"sinakashallid", "prillid":"Ei", "läätsed": "Ei", "aastaeg": "kevad", "värv":"lilla"}
-
-# kõik kokku
-kogumik = []
-
-# lisab neid
-kogumik.append(johannapapp)
-kogumik.append(johannasimm)
-
-f = open("kysimused.txt")
-
-# kysimuse valimine
-kyssad = []
-for rida in f:
-    k = f.readline()
-    kyssad.append(k)
-
-
-kysimus = random.choice(kyssad)
-
+f=open("vastused.txt")
+kysimus=f.readline().split("\t")
+kysimus.pop(0)
 print(kysimus)
-kyssad.remove(kysimus)
-vastus = input("Vasta 'Jah' või 'Ei':")
+
+for rida in f:
+    rida = rida.split("\t")
+    inimene = {}
+
+    for i in range(len(kysimus)):
+        inimene[kysimus[i]] = rida[i+1]
+
+print(inimene)
+                
