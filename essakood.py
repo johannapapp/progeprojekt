5aced2fbfb2d952457163689c58848eef3f4e15f
Administrator <johannapapp12@gmail.com>
2024-05-24 13:57:00 +0300

    koodiread

--- essakood.py
+++ essakood.py
@@ -1,13 +1,27 @@
-f=open("vastused.txt")
-kysimus=f.readline().split("\t")
+import random
+f = open("vastused.txt")
+kysimus = f.readline().split("\t")
 kysimus.pop(0)
-inimene = {}
-
+suur_sonastik = {}
 for rida in f:
     rida = rida.split("\t")
+    inimene = {}
 
     for i in range(len(kysimus)):
-        inimene[kysimus[i]] = rida[i+1]
+        inimene[kysimus[i]] = rida[i + 1]
+    name = rida[0]
+    suur_sonastik[name] = inimene
 
-print(inimene)
-                
+while len(kysimus) >= 1:
+    for x in kysimus:
+        question = (random.choice(kysimus))
+        print(question)
+        answer = input("Vasta 'Jah' või 'Ei'")
+        for nimi in suur_sonastik:
+            print(suur_sonastik[nimi])
+            if suur_sonastik[nimi][question] != answer:
+                print(nimi)
+
+
+print(kysimus)
+# print(suur_sonastik)
